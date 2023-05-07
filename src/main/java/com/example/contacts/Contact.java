4d399d1afb1ff54912110186b8d350fb7e9404b6
divomi (kysaadatlie) <saadat1orozova@gmail.com>
2023-05-08 01:59:28 +0600

    lll

---
+++ src/main/java/com/example/contacts/Contact.java
@@ -0,0 +1,54 @@
+package com.example.contacts;
+
+public class Contact {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String number;
+
+    public Contact(String firstName, String lastName, String email, String number){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.number = number;
+    }
+
+    public Contact(){}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return getLastName();
+    }
+}
--- src/main/java/com/example/contacts/ContactsController.java
+++ src/main/java/com/example/contacts/ContactsController.java
@@ -1,14 +1,21 @@
 package com.example.contacts;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
 
 public class ContactsController {
 
+    private ObservableList<Contact> contacts = FXCollections.observableArrayList();
+    private Contact contactSelected;
+
     @FXML
-    private ListView<?> contactsListView;
+    private ListView<Contact> contactsListView;
 
     @FXML
     private TextField emailTextField;
@@ -17,13 +24,96 @@ public class ContactsController {
     private TextField firstNameTextField;
 
     @FXML
-    private Pane infoView;
-
-    @FXML
     private TextField lastNameTextField;
 
     @FXML
     private TextField phoneNumberTextField;
 
+
+    public void initialize(){
+        contacts.add(new Contact("Jennie", "Kim", "kimjennie@gmail.com", "+9999999"));
+        contacts.add(new Contact("Saadat", "Orozova", "kysaadatlie@gmail.com", "+996770663780"));
+        contacts.add(new Contact("Killua", "Zoldyck", "zolkillua@gmail.com", "+999555222"));
+        contactsListView.setItems(contacts);
+
+        contactsListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Contact>() {
+            @Override
+            public void changed(ObservableValue<? extends Contact> observable, Contact oldValue, Contact newValue) {
+                firstNameTextField.setText(newValue.getFirstName());
+                lastNameTextField.setText(newValue.getLastName());
+                emailTextField.setText(newValue.getEmail());
+                phoneNumberTextField.setText(newValue.getNumber());
+            }
+        });
+    }
+
+    public boolean validationFailed() {
+        return (firstNameTextField.getText().trim().equals("") ||
+                lastNameTextField.getText().trim().equals("") ||
+                emailTextField.getText().trim().equals("") ||
+                phoneNumberTextField.getText().trim().equals(""));
+    }
+
+    @FXML
+    void addButtonPressed(ActionEvent event) {
+        try{
+            if (validationFailed()){
+                throw new NumberFormatException();
+            }
+            else{
+                Contact obj1 = new Contact();
+                obj1.setFirstName(firstNameTextField.getText());
+                obj1.setLastName(lastNameTextField.getText());
+                obj1.setEmail(emailTextField.getText());
+                obj1.setNumber(phoneNumberTextField.getText());
+                contacts.add(obj1);
+            }
+        } catch (Exception ex){
+            firstNameTextField.setText("");
+            lastNameTextField.setText("");
+            emailTextField.setText("");
+            phoneNumberTextField.setText("");
+            firstNameTextField.requestFocus();
+        }
+    }
+
+    @FXML
+    void deleteButtonPressed(ActionEvent event) {
+        try {
+            contacts.remove(contactSelected);
+        }
+        catch (Exception ex){
+            firstNameTextField.setText("");
+            lastNameTextField.setText("");
+            emailTextField.setText("");
+            phoneNumberTextField.setText("");
+            firstNameTextField.requestFocus();
+        }
+    }
+
+    @FXML
+    void updateButtonPressed(ActionEvent event) {
+        try{
+            if (validationFailed()){
+                throw new NumberFormatException();
+            }
+            else{
+                Contact obj1 = new Contact();
+                contactSelected.setFirstName(firstNameTextField.getText());
+                contactSelected.setLastName(lastNameTextField.getText());
+                contactSelected.setEmail(emailTextField.getText());
+                contactSelected.setNumber(phoneNumberTextField.getText());
+
+            }
+        } catch (Exception ex){
+            firstNameTextField.setText("");
+            lastNameTextField.setText("");
+            emailTextField.setText("");
+            phoneNumberTextField.setText("");
+            firstNameTextField.requestFocus();
+        }
+    }
+
 }
+
 
